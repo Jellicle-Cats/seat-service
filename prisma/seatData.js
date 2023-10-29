8e2bbba7a7bba320d3b0d64e0630bba94b814573
NonRoute <chanon.ratn@gmail.com>
2023-10-29 17:12:26 +0700

    Add more seat data

---
+++ prisma/seatData.js
@@ -0,0 +1,922 @@
+module.exports = seatData = [
+	{
+		floor: '1',
+		top: 282,
+		left: 927
+	},
+	{
+		floor: '1',
+		top: 303,
+		left: 927
+	},
+	{
+		floor: '1',
+		top: 316,
+		left: 927
+	},
+	{
+		floor: '1',
+		top: 339,
+		left: 927
+	},
+	{
+		floor: '1',
+		top: 352,
+		left: 927
+	},
+	{
+		floor: '1',
+		top: 374,
+		left: 927
+	},
+	{
+		floor: '1',
+		top: 387,
+		left: 927
+	},
+	{
+		floor: '1',
+		top: 408,
+		left: 927
+	},
+	{
+		floor: '1',
+		top: 425,
+		left: 927
+	},
+	{
+		floor: '1',
+		top: 447,
+		left: 927
+	},
+	{
+		floor: '1',
+		top: 282,
+		left: 895
+	},
+	{
+		floor: '1',
+		top: 303,
+		left: 895
+	},
+	{
+		floor: '1',
+		top: 316,
+		left: 895
+	},
+	{
+		floor: '1',
+		top: 339,
+		left: 895
+	},
+	{
+		floor: '1',
+		top: 352,
+		left: 895
+	},
+	{
+		floor: '1',
+		top: 374,
+		left: 895
+	},
+	{
+		floor: '1',
+		top: 387,
+		left: 895
+	},
+	{
+		floor: '1',
+		top: 408,
+		left: 895
+	},
+	{
+		floor: '1',
+		top: 425,
+		left: 895
+	},
+	{
+		floor: '1',
+		top: 447,
+		left: 895
+	},
+	{
+		floor: '1',
+		top: 282,
+		left: 883
+	},
+	{
+		floor: '1',
+		top: 303,
+		left: 883
+	},
+	{
+		floor: '1',
+		top: 316,
+		left: 883
+	},
+	{
+		floor: '1',
+		top: 339,
+		left: 883
+	},
+	{
+		floor: '1',
+		top: 352,
+		left: 883
+	},
+	{
+		floor: '1',
+		top: 374,
+		left: 883
+	},
+	{
+		floor: '1',
+		top: 387,
+		left: 883
+	},
+	{
+		floor: '1',
+		top: 408,
+		left: 883
+	},
+	{
+		floor: '1',
+		top: 425,
+		left: 883
+	},
+	{
+		floor: '1',
+		top: 447,
+		left: 883
+	},
+	{
+		floor: '1',
+		top: 282,
+		left: 855
+	},
+	{
+		floor: '1',
+		top: 303,
+		left: 855
+	},
+	{
+		floor: '1',
+		top: 316,
+		left: 855
+	},
+	{
+		floor: '1',
+		top: 339,
+		left: 855
+	},
+	{
+		floor: '1',
+		top: 352,
+		left: 855
+	},
+	{
+		floor: '1',
+		top: 374,
+		left: 855
+	},
+	{
+		floor: '1',
+		top: 387,
+		left: 855
+	},
+	{
+		floor: '1',
+		top: 408,
+		left: 855
+	},
+	{
+		floor: '1',
+		top: 425,
+		left: 855
+	},
+	{
+		floor: '1',
+		top: 447,
+		left: 855
+	},
+	{
+		floor: '1',
+		top: 282,
+		left: 843
+	},
+	{
+		floor: '1',
+		top: 303,
+		left: 843
+	},
+	{
+		floor: '1',
+		top: 316,
+		left: 843
+	},
+	{
+		floor: '1',
+		top: 339,
+		left: 843
+	},
+	{
+		floor: '1',
+		top: 352,
+		left: 843
+	},
+	{
+		floor: '1',
+		top: 374,
+		left: 843
+	},
+	{
+		floor: '1',
+		top: 387,
+		left: 843
+	},
+	{
+		floor: '1',
+		top: 408,
+		left: 843
+	},
+	{
+		floor: '1',
+		top: 425,
+		left: 843
+	},
+	{
+		floor: '1',
+		top: 447,
+		left: 843
+	},
+	{
+		floor: '1',
+		top: 316,
+		left: 788
+	},
+	{
+		floor: '1',
+		top: 339,
+		left: 788
+	},
+	{
+		floor: '1',
+		top: 352,
+		left: 788
+	},
+	{
+		floor: '1',
+		top: 374,
+		left: 788
+	},
+	{
+		floor: '1',
+		top: 387,
+		left: 788
+	},
+	{
+		floor: '1',
+		top: 408,
+		left: 788
+	},
+	{
+		floor: '1',
+		top: 425,
+		left: 788
+	},
+	{
+		floor: '1',
+		top: 447,
+		left: 788
+	},
+	{
+		floor: '1',
+		top: 316,
+		left: 776
+	},
+	{
+		floor: '1',
+		top: 339,
+		left: 776
+	},
+	{
+		floor: '1',
+		top: 352,
+		left: 776
+	},
+	{
+		floor: '1',
+		top: 374,
+		left: 776
+	},
+	{
+		floor: '1',
+		top: 387,
+		left: 776
+	},
+	{
+		floor: '1',
+		top: 408,
+		left: 776
+	},
+	{
+		floor: '1',
+		top: 425,
+		left: 776
+	},
+	{
+		floor: '1',
+		top: 447,
+		left: 776
+	},
+	{
+		floor: '1',
+		top: 497,
+		left: 916
+	},
+	{
+		floor: '1',
+		top: 518,
+		left: 916
+	},
+	{
+		floor: '1',
+		top: 552,
+		left: 916
+	},
+	{
+		floor: '1',
+		top: 573,
+		left: 916
+	},
+	{
+		floor: '1',
+		top: 611,
+		left: 916
+	},
+	{
+		floor: '1',
+		top: 633,
+		left: 916
+	},
+	{
+		floor: '1',
+		top: 666,
+		left: 916
+	},
+	{
+		floor: '1',
+		top: 688,
+		left: 916
+	},
+	{
+		floor: '1',
+		top: 726,
+		left: 916
+	},
+	{
+		floor: '1',
+		top: 748,
+		left: 916
+	},
+	{
+		floor: '1',
+		top: 497,
+		left: 893
+	},
+	{
+		floor: '1',
+		top: 518,
+		left: 893
+	},
+	{
+		floor: '1',
+		top: 552,
+		left: 893
+	},
+	{
+		floor: '1',
+		top: 573,
+		left: 893
+	},
+	{
+		floor: '1',
+		top: 611,
+		left: 893
+	},
+	{
+		floor: '1',
+		top: 633,
+		left: 893
+	},
+	{
+		floor: '1',
+		top: 666,
+		left: 893
+	},
+	{
+		floor: '1',
+		top: 688,
+		left: 893
+	},
+	{
+		floor: '1',
+		top: 726,
+		left: 893
+	},
+	{
+		floor: '1',
+		top: 748,
+		left: 893
+	},
+	{
+		floor: '1',
+		top: 492,
+		left: 863
+	},
+	{
+		floor: '1',
+		top: 509,
+		left: 863
+	},
+	{
+		floor: '1',
+		top: 526,
+		left: 863
+	},
+	{
+		floor: '1',
+		top: 545,
+		left: 863
+	},
+	{
+		floor: '1',
+		top: 562,
+		left: 863
+	},
+	{
+		floor: '1',
+		top: 579,
+		left: 863
+	},
+	{
+		floor: '1',
+		top: 606,
+		left: 863
+	},
+	{
+		floor: '1',
+		top: 624,
+		left: 863
+	},
+	{
+		floor: '1',
+		top: 677,
+		left: 863
+	},
+	{
+		floor: '1',
+		top: 694,
+		left: 863
+	},
+	{
+		floor: '1',
+		top: 721,
+		left: 863
+	},
+	{
+		floor: '1',
+		top: 738,
+		left: 863
+	},
+	{
+		floor: '1',
+		top: 756,
+		left: 863
+	},
+	{
+		floor: '1',
+		top: 773,
+		left: 863
+	},
+	{
+		floor: '1',
+		top: 492,
+		left: 851
+	},
+	{
+		floor: '1',
+		top: 509,
+		left: 851
+	},
+	{
+		floor: '1',
+		top: 526,
+		left: 851
+	},
+	{
+		floor: '1',
+		top: 545,
+		left: 851
+	},
+	{
+		floor: '1',
+		top: 562,
+		left: 851
+	},
+	{
+		floor: '1',
+		top: 579,
+		left: 851
+	},
+	{
+		floor: '1',
+		top: 606,
+		left: 851
+	},
+	{
+		floor: '1',
+		top: 624,
+		left: 851
+	},
+	{
+		floor: '1',
+		top: 677,
+		left: 851
+	},
+	{
+		floor: '1',
+		top: 694,
+		left: 851
+	},
+	{
+		floor: '1',
+		top: 721,
+		left: 851
+	},
+	{
+		floor: '1',
+		top: 738,
+		left: 851
+	},
+	{
+		floor: '1',
+		top: 756,
+		left: 851
+	},
+	{
+		floor: '1',
+		top: 773,
+		left: 851
+	},
+	{
+		floor: '1',
+		top: 499,
+		left: 798
+	},
+	{
+		floor: '1',
+		top: 517,
+		left: 798
+	},
+	{
+		floor: '1',
+		top: 553,
+		left: 798
+	},
+	{
+		floor: '1',
+		top: 572,
+		left: 798
+	},
+	{
+		floor: '1',
+		top: 614,
+		left: 798
+	},
+	{
+		floor: '1',
+		top: 631,
+		left: 798
+	},
+	{
+		floor: '1',
+		top: 669,
+		left: 798
+	},
+	{
+		floor: '1',
+		top: 687,
+		left: 798
+	},
+	{
+		floor: '1',
+		top: 499,
+		left: 787
+	},
+	{
+		floor: '1',
+		top: 517,
+		left: 787
+	},
+	{
+		floor: '1',
+		top: 553,
+		left: 787
+	},
+	{
+		floor: '1',
+		top: 572,
+		left: 787
+	},
+	{
+		floor: '1',
+		top: 614,
+		left: 787
+	},
+	{
+		floor: '1',
+		top: 631,
+		left: 787
+	},
+	{
+		floor: '1',
+		top: 669,
+		left: 787
+	},
+	{
+		floor: '1',
+		top: 687,
+		left: 787
+	},
+	{
+		floor: '1',
+		top: 813,
+		left: 911
+	},
+	{
+		floor: '1',
+		top: 813,
+		left: 889
+	},
+	{
+		floor: '1',
+		top: 813,
+		left: 848
+	},
+	{
+		floor: '1',
+		top: 813,
+		left: 828
+	},
+	{
+		floor: '1',
+		top: 813,
+		left: 805
+	},
+	{
+		floor: '1',
+		top: 813,
+		left: 782
+	},
+	{
+		floor: 'B',
+		top: 681,
+		left: 270
+	},
+	{
+		floor: 'B',
+		top: 694,
+		left: 270
+	},
+	{
+		floor: 'B',
+		top: 721,
+		left: 270
+	},
+	{
+		floor: 'B',
+		top: 734,
+		left: 270
+	},
+	{
+		floor: 'B',
+		top: 758,
+		left: 270
+	},
+	{
+		floor: 'B',
+		top: 771,
+		left: 270
+	},
+	{
+		floor: 'B',
+		top: 681,
+		left: 253
+	},
+	{
+		floor: 'B',
+		top: 694,
+		left: 253
+	},
+	{
+		floor: 'B',
+		top: 721,
+		left: 253
+	},
+	{
+		floor: 'B',
+		top: 734,
+		left: 253
+	},
+	{
+		floor: 'B',
+		top: 758,
+		left: 253
+	},
+	{
+		floor: 'B',
+		top: 771,
+		left: 253
+	},
+	{
+		floor: 'B',
+		top: 681,
+		left: 235
+	},
+	{
+		floor: 'B',
+		top: 694,
+		left: 235
+	},
+	{
+		floor: 'B',
+		top: 721,
+		left: 235
+	},
+	{
+		floor: 'B',
+		top: 734,
+		left: 235
+	},
+	{
+		floor: 'B',
+		top: 758,
+		left: 235
+	},
+	{
+		floor: 'B',
+		top: 771,
+		left: 235
+	},
+	{
+		floor: 'B',
+		top: 681,
+		left: 217
+	},
+	{
+		floor: 'B',
+		top: 694,
+		left: 217
+	},
+	{
+		floor: 'B',
+		top: 721,
+		left: 217
+	},
+	{
+		floor: 'B',
+		top: 734,
+		left: 217
+	},
+	{
+		floor: 'B',
+		top: 758,
+		left: 217
+	},
+	{
+		floor: 'B',
+		top: 771,
+		left: 217
+	},
+	{
+		floor: 'B',
+		top: 681,
+		left: 200
+	},
+	{
+		floor: 'B',
+		top: 694,
+		left: 200
+	},
+	{
+		floor: 'B',
+		top: 721,
+		left: 200
+	},
+	{
+		floor: 'B',
+		top: 734,
+		left: 200
+	},
+	{
+		floor: 'B',
+		top: 758,
+		left: 200
+	},
+	{
+		floor: 'B',
+		top: 771,
+		left: 200
+	},
+	{
+		floor: 'B',
+		top: 681,
+		left: 182
+	},
+	{
+		floor: 'B',
+		top: 694,
+		left: 182
+	},
+	{
+		floor: 'B',
+		top: 721,
+		left: 182
+	},
+	{
+		floor: 'B',
+		top: 734,
+		left: 182
+	},
+	{
+		floor: 'B',
+		top: 758,
+		left: 182
+	},
+	{
+		floor: 'B',
+		top: 771,
+		left: 182
+	},
+	{
+		floor: 'B',
+		top: 681,
+		left: 163
+	},
+	{
+		floor: 'B',
+		top: 694,
+		left: 163
+	},
+	{
+		floor: 'B',
+		top: 721,
+		left: 163
+	},
+	{
+		floor: 'B',
+		top: 734,
+		left: 163
+	},
+	{
+		floor: 'B',
+		top: 758,
+		left: 163
+	},
+	{
+		floor: 'B',
+		top: 771,
+		left: 163
+	},
+	{
+		floor: 'B',
+		top: 681,
+		left: 146
+	},
+	{
+		floor: 'B',
+		top: 694,
+		left: 146
+	},
+	{
+		floor: 'B',
+		top: 721,
+		left: 146
+	},
+	{
+		floor: 'B',
+		top: 734,
+		left: 146
+	},
+	{
+		floor: 'B',
+		top: 758,
+		left: 146
+	},
+	{
+		floor: 'B',
+		top: 771,
+		left: 146
+	}
+]
--- prisma/seed.js
+++ prisma/seed.js
@@ -1,49 +1,7 @@
 // npx prisma db seed
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
-
-const seatData = [
-	{
-		floor: '1',
-		top: 282,
-		left: 927
-	},
-	{
-		floor: '1',
-		top: 303,
-		left: 927
-	},
-	{
-		floor: '1',
-		top: 316,
-		left: 927
-	},
-	{
-		floor: '1',
-		top: 339,
-		left: 927
-	},
-	{
-		floor: '1',
-		top: 352,
-		left: 927
-	},
-	{
-		floor: '1',
-		top: 374,
-		left: 927
-	},
-	{
-		floor: '1',
-		top: 387,
-		left: 927
-	},
-	{
-		floor: '1',
-		top: 408,
-		left: 927
-	}
-]
+const seatData = require('./seatData')
 
 async function main() {
 	console.log(`Start seeding ...`)
